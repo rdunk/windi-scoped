3f87e400f94c723229079db298ad7cb188877360
Rupert Dunk <rupert@rupertdunk.com>
2021-02-23 14:40:32 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    Both lines below should appear blue when viewport width is above 639px.
+  </div>
+  <div class="scoped">SCOPED</div>
+  <div class="unscoped">UNSCOPED</div>
+</template>
+
+<style lang="postcss" scoped>
+.scoped {
+  @apply text-red-500;
+}
+
+@screen sm {
+  .scoped {
+    @apply text-blue-500;
+  }
+}
+</style>
+
+<style lang="postcss">
+.unscoped {
+  @apply text-red-500;
+}
+
+@screen sm {
+  .unscoped {
+    @apply text-blue-500;
+  }
+}
+</style>
+
+<style>
+#app {
+  font-family: sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
